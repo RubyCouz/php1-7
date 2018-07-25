11b586e46a7df9c5cf15381b5fb3628255e6a454
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 16:30:25 +0200

    exo8

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+<?php
+$calcul1 = 3+4;
+$calcul2 = 5*20;
+$calcul3 = 45/5;
+echo $calcul1 . ' ' . $calcul2 . ' ' . $calcul3;
+ ?>
+    </p>
+  </body>
+</html>
